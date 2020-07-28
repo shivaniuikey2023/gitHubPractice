469e5a33302ef3653c75ea7dbbceee4c0b8e2fa4
Shivani Uikey <shivani.5uikey@gmail.com>
2020-07-28 10:22:34 -0400

    Initial Commit

---
+++ gitHubPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitHubPractice
+//
+//  Created by Apple on 7/28/20.
+//  Copyright © 2020 Shivani Uikey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
